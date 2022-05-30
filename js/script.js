5f3c017cc142376fdd3c5cf879b508e875e25574
Martin PCescritorio <bergesio90@gmail.com>
2022-05-30 22:36:49 +0200

    iniciando tank you card

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 const $Numbers = document.querySelectorAll(".ratingWrapper__number"),
   $ButtonSubmit = document.querySelector(".buttonSubmit"),
-  $WrapperRatting = document.querySelector(".ratingState");
+  $WrapperRatting = document.querySelector(".ratingState"),
+  $WrapperThanks = document.querySelector(".thanksState"),
+  $ThanksStateP = document.querySelector(".thanksState__p");
+
+let ratting = 0;
 
 window.addEventListener("click", (e) => {
   $Numbers.forEach((el) => {
     el.classList.remove("ratingWrapper__number-selected");
     if (e.target === el) {
       el.classList.add("ratingWrapper__number-selected");
+      ratting = el.textContent;
     }
   });
 
   if (e.target === $ButtonSubmit) {
-    $WrapperRatting.style.display = "none";
+    $WrapperRatting.classList.add("display-none");
+    $WrapperThanks.classList.remove("display-none");
+    $ThanksStateP.innerHTML += `You selected ${ratting} out of 5`;
   }
 });
